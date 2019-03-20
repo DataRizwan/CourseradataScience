e3b63e54416089075a15fc82d7cb4b2ea614541c
RIZWAN MOHAMMED <rizwanahmed@in.ibm.com>
2019-03-20 20:08:50 +0530

    adding a python second file from ATOM and printing one statement

---
+++ python_second.py
@@ -0,0 +1,9 @@
+#Import the pandas library as pd.
+# useful for many scientific computing in Python
+import numpy as np
+
+#Import the Numpy library as np.
+# primary data structure library
+import pandas as pd
+
+print("Hello Capstone Project Course!")
